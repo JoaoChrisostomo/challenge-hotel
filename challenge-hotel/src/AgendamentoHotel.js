420405c7287cdadd91eea9dce954536da96193fc
Joao Chrisostomo <maturandomassas@gmail.com>
2022-05-07 00:30:14 -0300

    primeiro commit

---
+++ challenge-hotel/src/AgendamentoHotel.js
@@ -0,0 +1,9 @@
+export class AgendamentoHotel{
+    dataEntrada;
+    dataSaida;
+
+    constructor(dataEntrada, dataSaida){
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+    }
+}
---
+++ challenge-hotel/src/main.js
@@ -0,0 +1,17 @@
+// const hoteis = ["Lakewood", "Bridgewood", "Ridgewood"]
+// const classificaoDoHotel = [3, 4, 5]
+
+
+// function getCheapestHotel (input) { //DO NOT change the function's name.
+//     return "Cheapest hotel name"
+// }
+
+// exports.getCheapestHotel = getCheapestHotel
+
+
+import { Clientes } from './Clientes.js';
+import { Hotel } from './Hotel.js';
+import { AgendamentoHotel } from './AgendamentoHotel.js';
+
+const primeiroCliente = new Clientes("Lakewood", );
+
